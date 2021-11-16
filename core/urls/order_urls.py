51bfc36e120d975c4d55ad0bbebc9c1119714e2b
Ren0503 <illuminati0440@gmail.com>
2021-11-16 22:43:07 +0700

    Add product and order flow

---
+++ core/urls/order_urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from core.views import order_views as views
+
+
+urlpatterns = [
+
+    path('', views.getOrders, name='orders'),
+    path('add/', views.addOrderItems, name='orders-add'),
+    path('my_orders/', views.getMyOrders, name='my-orders'),
+
+    path('<str:pk>/deliver/', views.updateOrderToDelivered, name='order-delivered'),
+
+    path('<str:pk>/', views.getOrderById, name='user-order'),
+    path('<str:pk>/pay/', views.updateOrderToPaid, name='pay'),
+]
--- server/settings.py
+++ server/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'storages',
+
+    'core.apps.CoreConfig'
 ]
 
 REST_FRAMEWORK = {
