ddb4d099b2ef7ebd48705e712889800dd1f68e24
Ren0503 <illuminati0440@gmail.com>
2021-11-20 21:05:24 +0700

    Edit admin tech

--- admin/src/types/orders/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './Order';
-export * from './OrderDetail';
-export * from './OrderList';
-export * from './OrderPay';
-export * from './OrderDeliver';
--- admin/src/types/products/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Product';
-export * from './ProductList';
-export * from './ProductDetail';
-export * from './ProductCreate';
-export * from './ProductUpdate';
-export * from './ProductDelete';
-export * from './ProductTop';
